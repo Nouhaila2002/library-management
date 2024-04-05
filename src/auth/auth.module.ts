22eb3a1ec85570feb36cfbdb121fca6166942182
NOUHAILA EL OUADI <elouadinouhaila566@gmail.com>
2024-04-05 16:52:18 +0200

    second commit

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,17 +4,17 @@ import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './constants';
-import { config } from 'dotenv';
-import { join } from 'path';
+//import { config } from 'dotenv';
+//import { join } from 'path';
 
-config({ path: join(__dirname, '../.env') });
+//config({ path: join(__dirname, '../.env') });
 
 @Module({
   imports: [
     UsersModule,
     JwtModule.register({
       global: true,
-      secret: process.env.secret, //jwtConstants.secret,
+      secret: jwtConstants.secret,//process.env.secret, //jwtConstants.secret,
       signOptions: { expiresIn: '1h' },
     }),
   ],
